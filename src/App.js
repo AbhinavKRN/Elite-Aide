08810ecf69051afc1259ac985fc2900a5c2d3f49
AbhinavKRNarayan <abhinavv2005@gmail.com>
2024-10-08 16:27:30 +0530

    App file updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import WhyChoose from './components/WhyChoose';
+import Features from './components/Features';
+import FutureEnhancements from './components/FutureEnhancements';
+import Footer from './components/Footer';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <HeroSection />
+      <WhyChoose />
+      <Features />
+      <FutureEnhancements />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
