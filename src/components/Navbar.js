5aa8a0ac8bf8f09683a8d977ffc458d93f2e40a0
AbhinavKRNarayan <abhinavv2005@gmail.com>
2024-10-22 14:14:14 +0530

    Mobile responsive feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Navbar.css';
 import logo from '../assests/Vector-32.png'; 
 
 const Navbar = () => {
-    return (
-      <nav className="navbar">
-        <div className="logo-box">  
-          <img src={logo} alt="Elite Aide Logo" className="logo-image" />
-        </div>
-        <div className="nav-links">
-          <a href="#home" className="home-link">Home</a>
-          <a href="#about" className="about-link">About</a>
-          <a href="#contact" className="contact-link">Contact Us</a>
-          <a href="#download" className="download-link">Download the App</a>
-        </div>
-      </nav>
-    );
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
   };
-  
-  export default Navbar;
+
+  return (
+    <nav className={`navbar ${isNavOpen ? 'nav-open' : ''}`}>
+      <div className="logo-box">  
+        <img src={logo} alt="Elite Aide Logo" className="logo-image" />
+      </div>
+      <div className="hamburger" onClick={toggleNav}>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="nav-links">
+        <a href="#home" onClick={toggleNav} className="home-link">Home</a>
+        <a href="#about" onClick={toggleNav} className="about-link">About</a>
+        <a href="#contact" onClick={toggleNav} className="contact-link">Contact Us</a>
+        <a href="#download" onClick={toggleNav} className="download-link">Download the App</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
